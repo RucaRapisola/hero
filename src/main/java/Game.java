1ee10de2b32364676edf86e195eabdff57a0ab2b
RucaRapisola <ninjadeouro12@gmail.com>
2022-10-04 15:13:00 +0100

    mobilização do heroi

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,6 +10,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
+    private int x = 10;
+    private int y = 10;
     Screen screen;
     Game() throws IOException {
         try {
@@ -23,12 +27,36 @@ public class Game {
             e.printStackTrace();
         }
     }
+    private void processKey(KeyStroke key) {
+        switch(key.getKeyType()){
+            case ArrowUp:
+                y -= 1;
+                break;
+            case ArrowDown:
+                y+=1;
+                break;
+            case ArrowRight:
+                x+=1;
+                break;
+            case ArrowLeft:
+                x-=1;
+                break;
+        }
+    }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() throws IOException {
-        draw();
+        while(true){
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
     }
 }
